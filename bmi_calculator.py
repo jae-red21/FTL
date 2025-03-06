0f1534a916d3c6c74ab2b79788ae86a5661dc7c8
jae-red21 <jared21h25eagle@gmail.com>
2025-03-06 23:05:58 +0300

    python implemented BMI calculator

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -1,13 +1,40 @@
-def bmi_calculator(height:float, weight: float) -> float:
-    return weight / (height ** 2)
+calculate_bmi = lambda height, weight: weight/(height*height)
 
 def main():
     print("Welcome to the BMI Calculator!")
     while True:
-        weight = float(input("Enter weight in kilograms: "))
-        height = float(input("Enter height in meters: "))
-        bmi: float = bmi_calculator(weight, height)
+        while True:
+            try:
+                weight = float(input("Enter weight in kilograms: "))
+                
+                if weight <= 0.0:
+                    raise ValueError("Weight must be greater than zero.")
+                break
+            except ValueError as e:
+                print(f"Error: {e} Please Try again")
+        
+        while True:
+            try:
+                height = float(input("Enter height in meters: "))
+                
+                if height <= 0.0:
+                    raise ValueError("Height must greater than zero.")
+                break
+            except ValueError as e:
+                print(f"Invalid input: {e} Please try again")
+                    
+            
+        bmi: float = calculate_bmi(height, weight)
         print("Your BMI is: ", bmi)
+        
+        if bmi < 18.5:
+            print("Your are underweight.")
+        elif bmi < 25:
+            print("Your are normal weight.")
+        elif bmi < 30:
+            print("Your are overweight.")
+        else:
+            print("Your are Obese.")
         response = input("Would you like to try again? (y/n): ").strip().lower()[0]
         if response != "y":
             print("Thank you for using BMI Calculator!")
